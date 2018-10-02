1174fcf3011d9fa060429f7ad56431a5b372bf44
Ryan Smith <rms1000watt@gmail.com>
2018-10-02 14:12:18 -0700

    playing with pipeline

--- main.go
+++ main.go
@@ -5,41 +5,55 @@ import (
 )
 
 func main() {
+	skipArgs := sdk.Arguments{
+		{
+			Description: "Skip this step",
+			Type:        sdk.BoolInp,
+			Key:         "skip",
+		},
+	}
+
 	jobs := sdk.Jobs{
 		{
 			Handler:     jobTag,
 			Title:       "Tag",
 			Description: "This tags all the github repos",
+			Args:        skipArgs,
 		},
 		{
 			Handler:     jobEnv,
 			Title:       "Env",
 			Description: "This sets up an environment",
 			DependsOn:   []string{"Tag"},
+			Args:        skipArgs,
 		},
 		{
 			Handler:     jobBuild,
 			Title:       "Build",
 			Description: "This builds everything",
 			DependsOn:   []string{"Env"},
+			Args:        skipArgs,
 		},
 		{
 			Handler:     jobDeploy,
 			Title:       "Deploy",
 			Description: "This will deploy everything",
 			DependsOn:   []string{"Build"},
+			Args:        skipArgs,
 		},
 		{
 			Handler:     jobDestroy,
 			Title:       "Destroy",
 			Description: "This will destroy the environment",
 			DependsOn:   []string{"Deploy"},
+			Args:        skipArgs,
 		},
 		{
 			Handler:     jobPR,
 			Title:       "PR",
 			Description: "This will make the PR",
 			DependsOn:   []string{"Destroy"},
+			Args:        skipArgs,
 		},
 	}
 
